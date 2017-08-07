0941eca0dd036c71d1a9aba3b99d5f31c955eba6
Abhimanyue Singh Tanwar <abhimanyu891998@users.noreply.github.com>
2017-08-08 04:41:54 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,5 +4,5 @@ var count = 0;
 counter.onclick = function(){
     count = count+1;
     var countname = document.getElementById('count');
-    countname.InnderHTML = count.toString();
+    countname.InnerHTML = count.toString();
     };
