d3b4e3c6f44b7be657fb2e224a1f9bc1558b467e
Abhimanyue Singh Tanwar <abhimanyu891998@users.noreply.github.com>
2017-08-08 03:32:57 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,13 @@
 console.log('Loaded!');
 var img = document.getElementById('tech');
-img.onclick = function(){
+marginleft = 0;
+function moveright(){
+    marginleft = marginleft + 10;
+    img.style.marginLeft = marginleft + 'px';
     
+    
+}
+img.onclick = function(){
+    var interval = setInterval(moveright,100);
     img.style.marginLeft = '100px';
     };
