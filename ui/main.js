a0c81b4bf4a70df4fde97ceb5a49abfa1b0018ce
Abhimanyue Singh Tanwar <abhimanyu891998@users.noreply.github.com>
2017-08-08 03:35:34 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var img = document.getElementById('tech');
 marginleft = 0;
 function moveright(){
-    marginleft = marginleft + 10;
+    marginleft = marginleft + 1;
     img.style.marginLeft = marginleft + 'px';
     
     
