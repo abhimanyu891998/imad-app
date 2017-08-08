ff2038707f76c80732b990fa0cbd5f0ae76b85f8
Abhimanyue Singh Tanwar <abhimanyu891998@users.noreply.github.com>
2017-08-09 00:07:45 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -66,45 +66,6 @@ request.send(null);
 
 // js code for article pages
 
-var comment_button = document.getElementById('submit_cmt');
-comment_button.onclick = function(){
-    var comment_text = document.getElementById('cmt_txt');
-    var comment_content = comment_text.value;
-    
-        var request = new XMLHttpRequest();
-request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE ){
-        
-        if(request.status === 200){
-            
-            
-    var comments = request.responseText;
-    comments = JSON.parse(comments);
-    
-    var list = '';
-    for(var i =0 ; i<names.length ; i++)
-    {
-        list += '<li>'+names[i]+'</li>'; 
-        
-        
-    }
-var ul = document.getElementById('cmt_list');
-ul.innerHTML = list;
-
-    
-           
-            
-        }
-    }
-    
-    
-};
-request.open('GET', 'http://singhabhimanyu186.imad.hasura-app.io/comment?comment='+comment_content, true);
-request.send(null);
-    
-    
-    
-};
 
 
 
