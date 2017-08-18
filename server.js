cff3b1d41f5b9dccc8b8ef60b710b10c410427ee
Abhimanyue Singh Tanwar <abhimanyu891998@users.noreply.github.com>
2017-08-18 16:36:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -227,7 +227,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 function hash(input,salt){
     
-    var hashed = crypto.pbkdf25Sync(input,salt,10000,512,'sha512');
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return hashed.toString('hex');
 }
 
